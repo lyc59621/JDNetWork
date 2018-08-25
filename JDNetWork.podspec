7315595847fdbb9f3b83a7f6497a1dfa213b2c54
姜锦龙 <lyc59621@icloud.com>
2018-08-25 16:18:21 +0800

    edit JDNetWork.podspec

--- JDNetWork.podspec
+++ JDNetWork.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "JDNetWork"
-  s.version      = "2"
+  s.version      = "0.0.1"
   s.summary      = "JDNetWork."
 
   # This description is used to generate tags and improve search results.
--- upload.sh
+++ upload.sh
@@ -1,21 +1,63 @@
 #!/bin/bash
 
+Cyan='\033[0;36m'
+Default='\033[0;m'
+
+versionName=""
+commitContent=""
+confirmed="n"
+
+getVersionName() {
+read -p "Enter Version Name: " versionName
+
+if test -z "$versionName"; then
+getVersionName
+fi
+}
+
+getCommitContent() {
+read -p "Enter Commit  Content: " commitContent
+
+if test -z "$commitContent"; then
+commitContent
+fi
+}
+getInfomation() {
+getVersionName
+getCommitContent
+
+echo -e "\n${Default}================================================"
+echo -e "  Version Name      :  ${Cyan}${versionName}${Default}"
+echo -e "  Commit  Content   :  ${Cyan}${commitContent}${Default}"
+echo -e "================================================\n"
+}
+echo -e "\n"
+while [ "$confirmed" != "y" -a "$confirmed" != "Y" ]
+do
+if [ "$confirmed" == "n" -o "$confirmed" == "N" ]; then
+getInfomation
+fi
+read -p "confirm? (y/n):" confirmed
+done
+
 git stash
 git pull origin master --tags
 git stash pop
 
+
 VersionString=`grep -E 's.version.*=' JDNetWork.podspec`
 VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
 
-NewVersionNumber=$(($VersionNumber + 1))
+VersionString=`grep -E 's.version.*=' JDNetWork.podspec`
 LineNumber=`grep -nE 's.version.*=' JDNetWork.podspec | cut -d : -f1`
-sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" JDNetWork.podspec
+sed -i "" "${LineNumber}s/${VersionNumber}/${versionName}/g" JDNetWork.podspec
 
-echo "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
 
 git add .
-git commit -am ${NewVersionNumber}
-git tag ${NewVersionNumber}
+git commit -a -m ${commitContent}
+git tag ${versionName}
 git push origin master --tags
-#cd ~/.cocoapods/repos/CasaPasswordRepositoryStore && git pull origin master && cd - && #pod repo push CasaPasswordRepositoryStore JDNetWork.podspec --verbose --allow-#warnings --use-libraries
+
+git pull origin master && pod repo push JDNetWork.podspec --verbose --allow- #warnings --use-libraries
+
 
